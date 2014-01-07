3c95f82d2e9ddc2e4b9c06113c6db5174fc1c942
Arthur Nogueira Neves <arthurnn@gmail.com>
2014-01-07 14:17:41 -0500

    Update secret_token.rb
    rotate keys

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-PicturmeRails::Application.config.secret_token = '386f91b48254a8058b94f8da2cd21a82a3c8ac4c6c53524a6cd698ae6afdf111d8360bc92b5dee50062d65ec5e9f12c7e1d4d0ae9614ed272b5fd439ee7bbb45'
+PicturmeRails::Application.config.secret_token = ENV['secret_token']
