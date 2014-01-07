2e31255070b120378143c77e5337112e8d0837d2
Arthur Nogueira Neves <arthurnn@gmail.com>
2014-01-07 14:18:33 -0500

    Update carrierwave.rb
    rotate keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,8 +3,8 @@ CarrierWave.configure do |config|
   config.storage = :s3
   config.fog_credentials = {
     :provider               => 'AWS',       # required
-    :aws_access_key_id      => 'AKIAIC6VFIJCTGUNGU4A',       # required
-    :aws_secret_access_key  => '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',       # required
+    :aws_access_key_id      => ENV['AWS_KEY_ID']
+    :aws_secret_access_key  => ENV['AWS_ACCESS_KEY']
   }
   config.fog_directory  = 'picturme_bucket'                     # required
   config.fog_host       = 'http://s3.amazonaws.com/picturme_bucket'   # optional, defaults to nil
